da54bf097633253f49763c9259dc09c47517df00
Herman Havva <108483440+hermanhavva@users.noreply.github.com>
2024-09-17 01:26:47 +0300

    color added

--- FlightBooker/Source.cpp
+++ FlightBooker/Source.cpp
@@ -8,11 +8,13 @@ using namespace std;
     
 
 
+HANDLE hout = GetStdHandle(STD_OUTPUT_HANDLE);
 
 int main ()
 {
 
-	
+	SetConsoleTextAttribute(hout, BACKGROUND_INTENSITY | BACKGROUND_RED | BACKGROUND_GREEN);
+	system("cls");
 	TicketManager* ticketmanager = new TicketManager();
 	chrono::year_month_day date{ chrono::year(2024) / chrono::month(9) / chrono::day(14) };
 	
